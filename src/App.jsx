7fdde4880207ea9946bab0b92c8b3d8563a6d437
Sibangee Mohanty <129550788+SibangeeM@users.noreply.github.com>
2024-03-29 16:15:58 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
-import { CartContextProvider } from "./store/cartcontext";
+import { CartContextProvider } from "./store/cartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
 
 function App() {
