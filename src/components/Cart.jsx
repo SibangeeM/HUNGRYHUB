c4ff2b2950eb831f54903e2eb3faa679083c4f33
Sibangee Mohanty <129550788+SibangeeM@users.noreply.github.com>
2024-03-29 16:20:41 -0500

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "../UI/Modal";
 import { useContext } from "react";
-import CartContext from "../store/cartcontext";
+import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "../UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
